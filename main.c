a0b2f7cb5225450dbf0ec5c3c7a2788105715b9b
Screenyx <mercier.hugo14@gmail.com>
2021-02-26 10:57:37 +0100

    tests on threads

--- main.c
+++ main.c
@@ -1,6 +1,34 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define NB_THREADS 5
+
+void * fonction_thread(void * arg);
 
 int main(void){
-    
+    pthread_t thr[NB_THREADS];
+    int i;
+    for (int i = 0; i < NB_THREADS; i++)
+    {
+        if(pthread_create(&thr[i], NULL, fonction_thread, (void *)i)!=0){
+            fprintf(stderr, "Erreur dans pthread_create\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+    while (1)
+    {
+        fprintf(stderr,"Thread Main\n");
+        sleep(1);
+    }
+}
+
+void * fonction_thread(void * arg) {
+    int num = (int) arg;
+    while (1) {
+        fprintf(stderr, "Thread numero %d, id %d\n", num+1,pthread_self());
+        sleep(1);
+    }
 }
